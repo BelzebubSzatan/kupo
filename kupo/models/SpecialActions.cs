3f7ad3f47f6af3e34c8537bdd06a6f21edf4a412
KacperMatlag <matlagkacper@gmail.com>
2024-03-04 21:05:46 +0100

    Add special actions enum

---
+++ kupo/models/SpecialActions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kupo.models {
+    public enum SpecialActions {
+        Normal=1,
+        AddTwo,
+        AddFour,
+        Color,
+    }
+}
