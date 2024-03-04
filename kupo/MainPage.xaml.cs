d8b8ee175834de391b86faf1a8feba375717aae5
KacperMatlag <matlagkacper@gmail.com>
2024-03-04 21:26:18 +0100

    Basic c# backend layout

--- kupo/MainPage.xaml.cs
+++ kupo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kupo.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,10 +9,38 @@ using Xamarin.Forms;
 
 namespace kupo {
     public partial class MainPage : ContentPage {
+        Deck deck=new Deck();
+        List<Card> playerCards = new List<Card>();
+        List<Card> computerCards = new List<Card>();
+        Card LastCards = null;
+        bool win=false;
+        bool playerAction = true;
         public MainPage() {
             InitializeComponent();
+            playerCards = deck.GetPlayerCards(3);
+            computerCards = deck.GetPlayerCards(3);
+            RenderPlayerCards();
+            SetLastCard(null);
         }
 
+        public void RenderPlayerCards() {
+
+        }
+        private void PlayerCardClick(object sender, EventArgs e) {
+
+        }
+        private void Wincheck() {
+
+        }
+        private void SetLastCard(Card c) {
+
+        }
+        public void ComputerMove() {
+
+        }
+        public void SpecialCardAction(List<Card> target,Card c) {
+
+        }
         private void Button_Clicked(object sender, EventArgs e)
         {
 
